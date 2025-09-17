eb300c909035446bf09d1fc7eb6318cd2716c29d
PAOLA MARISOL PÉREZ LÓPEZ <paola.perez24@itca.edu.sv>
2025-09-16 19:54:06 -0600

    Creacion del html nav

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,12 +1,24 @@
 <template>
 
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>Conciertos</li>
-            <li>Albumes</li>
-            <li>Contactanos</li>
-        </ul>
+    <nav class="navbar">
+        <div class="nav-container">
+            <a href="#" class="logo">
+                <img src="https://crystalpng.com/wp-content/uploads/2023/04/BTS-Logo.png" alt="">
+            </a>
+            
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#inicio" class="nav-link active">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#conciertos" class="nav-link">Conciertos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#albumes" class="nav-link">Álbumes</a>
+                </li>
+            </ul>
+            
+        </div>
     </nav>
 
 </template>
@@ -16,8 +28,5 @@
 </script>
 
 <style>
-*{
-    background-color: blueviolet;
-    font-family: 'Courier New', Courier, monospace;
-}
+
 </style>
